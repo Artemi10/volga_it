173016c2e2b8b4b7a60e2c39cdfdf4dda65c5ee4
lyaha <lyah.artem10@mailru>
2021-09-28 21:39:52 +0300

    bug fixing

--- src/main/java/devanmejia/service/parser/HTMLLineParser.java
+++ src/main/java/devanmejia/service/parser/HTMLLineParser.java
@@ -1,15 +1,20 @@
 package devanmejia.service.parser;
 
 
+import org.apache.commons.text.*;
+
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class HTMLLineParser implements LineParser {
     @Override
     public Stream<String> parse(String line){
-        line += "</tag>";
+        line = StringEscapeUtils.unescapeHtml4(line) + "</tag>";
         return Arrays.stream(line.replaceAll("<.*?>", "")
-                        .split("&([^;]*);|\\s|[^a-zA-Z0-9А-Яа-я]"))
-                .filter(word -> !word.trim().equals(""));
+                        .replaceAll(String.valueOf((char) 160), " ")
+                        .split(",|\\.|!|\\?|\"|;|:|\\[|]|\\(|\\)|\\s"))
+                .map(String::trim)
+                // check if word is not empty and contains at least one letter or number
+                .filter(word -> !word.equals("") && word.matches(".*\\d|[a-zA-Zа-яА-Я]+.*"));
     }
 }
--- src/test/java/devanmejia/service/parser/HTMLLineParserTest.java
+++ src/test/java/devanmejia/service/parser/HTMLLineParserTest.java
@@ -16,7 +16,7 @@ class HTMLLineParserTest {
     }
 
     @Test
-    void parse_empty_line_test() {
+    public void parse_empty_line_test() {
         String emptyLine1 = lineParser.parse("").collect(Collectors.joining());
         String emptyLine2 = lineParser.parse(" ").collect(Collectors.joining());
         Assertions.assertEquals("", emptyLine1);
@@ -24,40 +24,44 @@ class HTMLLineParserTest {
     }
 
     @Test
-    void parse_empty_text_test() {
-        String emptyLine1 = lineParser.parse("<div></div>").collect(Collectors.joining());
-        String emptyLine2 = lineParser.parse("<div> </div>").collect(Collectors.joining());
-        String emptyLine3 = lineParser.parse("<div class=\"row\"> </div>").collect(Collectors.joining());
-        String emptyLine4 = lineParser.parse("<input type=\"text\">").collect(Collectors.joining());
-        String emptyLine5 = lineParser.parse("<div>&nbsp; </div>").collect(Collectors.joining());
-        String emptyLine6 = lineParser.parse("<div>&nbsp;</div>").collect(Collectors.joining());
-        Assertions.assertEquals("", emptyLine1);
-        Assertions.assertEquals("", emptyLine2);
-        Assertions.assertEquals("", emptyLine3);
-        Assertions.assertEquals("", emptyLine4);
-        Assertions.assertEquals("", emptyLine5);
-        Assertions.assertEquals("", emptyLine6);
+    public void parse_empty_text_test() {
+        String line1 = lineParser.parse("<div></div>").collect(Collectors.joining());
+        String line2 = lineParser.parse("<div> </div>").collect(Collectors.joining());
+        String line3 = lineParser.parse("<div class=\"row\"> </div>").collect(Collectors.joining());
+        String line4 = lineParser.parse("<input type=\"text\">").collect(Collectors.joining());
+        String line5 = lineParser.parse("<div>&nbsp; </div>").collect(Collectors.joining());
+        String line6 = lineParser.parse("<div>&nbsp;</div>").collect(Collectors.joining());
+        Assertions.assertEquals("", line1);
+        Assertions.assertEquals("", line2);
+        Assertions.assertEquals("", line3);
+        Assertions.assertEquals("", line4);
+        Assertions.assertEquals("", line5);
+        Assertions.assertEquals("", line6);
     }
 
     @Test
-    void parse_text_with_special_symbols_test(){
-        String emptyLine1 = lineParser.parse("<div>How&nbsp;are&nbsp;you</div>").collect(Collectors.joining(", "));
-        String emptyLine2 = lineParser.parse("<div>How&nbsp;are</div>").collect(Collectors.joining(", "));
-        String emptyLine3 = lineParser.parse("<div>&nbsp;How&nbsp;&nbsp;&nbsp;are&nbsp;</div>").collect(Collectors.joining(", "));
-        Assertions.assertEquals("How, are, you", emptyLine1);
-        Assertions.assertEquals("How, are", emptyLine2);
-        Assertions.assertEquals("How, are", emptyLine3);
+    public void parse_text_with_special_symbols_test(){
+        String line1 = lineParser.parse("<div>How&nbsp;are&nbsp;you</div>").collect(Collectors.joining(", "));
+        String line2 = lineParser.parse("<div>How&nbsp;are</div>").collect(Collectors.joining(", "));
+        String line3 = lineParser.parse("<div>&nbsp;How&nbsp;&nbsp;&nbsp;are&nbsp;</div>").collect(Collectors.joining(", "));
+        Assertions.assertEquals("How, are, you", line1);
+        Assertions.assertEquals("How, are", line2);
+        Assertions.assertEquals("How, are", line3);
     }
 
     @Test
-    void parse_text_with_nested_tags_test(){
-        String emptyLine1 = lineParser.parse("<div>How <span>are</span> you</div>").collect(Collectors.joining(", "));
-        String emptyLine2 = lineParser.parse("<div>How<span>are</span> you</div>").collect(Collectors.joining(", "));
-        String emptyLine3 = lineParser.parse("<div>How<span>are</span>you</div>").collect(Collectors.joining(", "));
-        String emptyLine4 = lineParser.parse("<div>How&nbsp;<span>are</span>&nbsp;you</div>").collect(Collectors.joining(", "));
-        Assertions.assertEquals("How, are, you", emptyLine1);
-        Assertions.assertEquals("Howare, you", emptyLine2);
-        Assertions.assertEquals("Howareyou", emptyLine3);
-        Assertions.assertEquals("How, are, you", emptyLine4);
+    public void parse_text_with_nested_tags_test(){
+        String line1 = lineParser.parse("<div>How <span>are</span> you</div>").collect(Collectors.joining(", "));
+        String line2 = lineParser.parse("<div>How<span>are</span> you</div>").collect(Collectors.joining(", "));
+        String line3 = lineParser.parse("<div>How<span>are</span>you</div>").collect(Collectors.joining(", "));
+        String line4 = lineParser.parse("<div>How&nbsp;<span>are</span>&nbsp;you</div>").collect(Collectors.joining(", "));
+        String line5 = lineParser.parse("<li>&nbsp;Chapter 5 of Roy Fielding&rsquo;s dissertation&nbsp;<a rel=\"nofollow\" href=\"http://www.ics.uci.edu/~fielding/pubs/dissertation/rest_arch_style.htm\">&laquo;Representational State Transfer (REST)&raquo;</a></li>\n")
+                .collect(Collectors.joining(", "));
+        Assertions.assertEquals("How, are, you", line1);
+        Assertions.assertEquals("Howare, you", line2);
+        Assertions.assertEquals("Howareyou", line3);
+        Assertions.assertEquals("How, are, you", line4);
+        Assertions.assertEquals("Chapter, 5, of, Roy, Fielding’s, dissertation, State, Transfer, REST", line5);
     }
+
 }
